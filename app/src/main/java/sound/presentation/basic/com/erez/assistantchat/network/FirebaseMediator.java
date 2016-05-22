1ca33d227b6ab8b771e3b72aaece62a2dd47da88
erez <erez2atar@gmail.com>
2016-05-22 11:27:31 +0300

    init

--- app/src/main/java/sound/presentation/basic/com/erez/assistantchat/network/FirebaseMediator.java
+++ app/src/main/java/sound/presentation/basic/com/erez/assistantchat/network/FirebaseMediator.java
@@ -1,8 +1,21 @@
 package sound.presentation.basic.com.erez.assistantchat.network;
 
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 /**
  * Created by LENOVO on 22/05/2016.
  */
 public class FirebaseMediator implements IServerMediator
 {
+
+    private static final String FIREBASE_ADDRESS = "https://incandescent-inferno-5809.firebaseio.com";
+    private static final String ACTIVE_ASSISTANTS_CHILD = "active_assistants";
+    private static final String ASSISTANTS_DETAILS_CHILD = "assistants";
+    private static final String OPENED_SESSIONS_CHILD = "opened_sessions";
+
+    private Firebase fb;
+
+
 }
