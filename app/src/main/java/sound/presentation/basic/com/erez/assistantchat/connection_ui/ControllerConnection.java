66e75fb6d1380d4c305a541892167fe6f7382583
erez <erez2atar@gmail.com>
2016-05-23 10:44:01 +0300

    k

--- app/src/main/java/sound/presentation/basic/com/erez/assistantchat/connection_ui/ControllerConnection.java
+++ app/src/main/java/sound/presentation/basic/com/erez/assistantchat/connection_ui/ControllerConnection.java
@@ -3,6 +3,7 @@ package sound.presentation.basic.com.erez.assistantchat.connection_ui;
 import android.content.Intent;
 
 import sound.presentation.basic.com.erez.assistantchat.chat_ui.ChatActivity;
+import sound.presentation.basic.com.erez.assistantchat.chat_ui.ChatActivityLastMsg;
 import sound.presentation.basic.com.erez.assistantchat.misc.App;
 import sound.presentation.basic.com.erez.assistantchat.network.IServerMediator;
 
@@ -16,7 +17,7 @@ public class ControllerConnection implements IServerMediator.OpenSessionsListene
     public void onChatOpened()
     {
         App.getServerMediator().changeAvailableStatus(false);
-        Intent intent = new Intent(App.getInstance(), ChatActivity.class);
+        Intent intent = new Intent(App.getInstance(), ChatActivityLastMsg.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         App.getInstance().startActivity(intent);
     }
