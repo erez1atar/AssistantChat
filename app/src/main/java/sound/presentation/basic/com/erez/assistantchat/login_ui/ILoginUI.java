e7a226a177c386c582d4ef0c2a56b0554c25b939
Tom Vizel <tomvizelsmt@gmail.com>
2016-05-29 12:42:31 +0300

    LoginActivity now communicates with controller

---
+++ app/src/main/java/sound/presentation/basic/com/erez/assistantchat/login_ui/ILoginUI.java
@@ -0,0 +1,11 @@
+package sound.presentation.basic.com.erez.assistantchat.login_ui;
+
+/**
+ * Created by Tom Vizel on 29/05/2016.
+ */
+public interface ILoginUI
+{
+    String getName();
+
+    String getPassword();
+}
