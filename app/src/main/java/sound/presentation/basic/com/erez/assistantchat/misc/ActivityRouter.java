eaa8e5abf691696abce854efd6fba0ad3351ca74
erez <erez2atar@gmail.com>
2016-05-30 09:19:18 +0300

    Merge branch 'master' of https://github.com/erez1atar/AssistantChat
    Conflicts:
    	app/src/main/java/sound/presentation/basic/com/erez/assistantchat/connection_ui/ConnectionActivity.java

--- app/src/main/java/sound/presentation/basic/com/erez/assistantchat/misc/ActivityRouter.java
+++ app/src/main/java/sound/presentation/basic/com/erez/assistantchat/misc/ActivityRouter.java
@@ -3,6 +3,7 @@ package sound.presentation.basic.com.erez.assistantchat.misc;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 /**
  * Class for allowing a simple change from one activity to the next.
@@ -11,6 +12,7 @@ public class ActivityRouter
 {
     public static void changeActivity(Context context, Class<? extends Activity> activity)
     {
+        Log.d("ActivityRouter", "changeActivity" + activity.toString());
         Intent intent = new Intent(context, activity);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
